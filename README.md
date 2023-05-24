54261e2d2efda59677ebc56144af0d0473e2e528
BoubakerBOUSEBAA <boubakerbousebaa@gmail.com>
2023-05-24 17:14:50 +0200

    projet terminer possibilité d'améliorer le css

---
+++ README.md
@@ -0,0 +1,15 @@
+# Project Carrousel React
+
+Le carrousel devrait permettre à l'utilisateur de cliquer sur le bouton Précédent ou Suivant pour passer à l'image précédente ou suivante.
+
+Les images seront stockées dans un tableau simple.
+
+Si vous ne souhaitez pas utiliser d'images, vous pouvez également utiliser du texte pour créer un carrousel de témoignages qui parcourt les avis pour un produit donné.
+
+Enfin, si vous souhaitez faire passer votre carrousel au niveau supérieur, essayez d'ajouter une transition amusante à l'aide de CSS pour animer l'image au fur et à mesure qu'elle change.
+
+## React concepts utilisés
+
+- useState (stockage et mise à jour de l'état)
+- Conditionnels (ternaires)
+- Listes, clés et .map()
--- src/App.js
+++ src/App.js
@@ -1,25 +1,67 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./styles.css";
+import paysage1 from "./images/paysage_1.png";
+import paysage2 from "./images/paysage_2.png";
+import paysage3 from "./images/paysage_3.png";
+import paysage4 from "./images/paysage_4.png";
+import paysage5 from "./images/paysage_5.png";
 
-function App() {
+const images = [
+
+  //En utilisant les images provenant de l'API Pexels
+ // "https://images.pexels.com/photos/3836292/pexels-photo-3836292.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
+  //"https://images.pexels.com/photos/2792157/pexels-photo-2792157.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
+  //"https://images.pexels.com/photos/1166209/pexels-photo-1166209.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
+  //"https://images.pexels.com/photos/1661179/pexels-photo-1661179.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
+  //"https://images.pexels.com/photos/45201/kitty-cat-kitten-pet-45201.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500"
+
+  //En utilisant les images provenant du dossier images
+
+  paysage1,
+  paysage2,
+  paysage3,
+  paysage4,
+  paysage5
+
+];
+
+
+
+
+export default function App() {
+  const [current, setCurrent] = useState(0);
+
+  function nextSlide() {
+    setCurrent(current === images.length - 1 ? 0 : current + 1);
+  }
+
+  function prevSlide() {
+    setCurrent(current === 0 ? images.length - 1 : current - 1);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div class="slider-thumb">
+    <div>
+      <h2>React 1: Carrousel</h2>
+      <div className="slider">
+        <div className="left-arrow" onClick={prevSlide}>
+          ⬅
+        </div>
+        <div className="right-arrow" onClick={nextSlide}>
+          ⮕
+        </div>
+        {images.map(
+          (image, index) =>
+            current === index && (
+              <div key={image} className="slide">
+                <img src={image} alt="images" />
+              </div>
+    
+    
+            )
+        )}
+      </div>
+    </div>
     </div>
   );
 }
-
-export default App;
